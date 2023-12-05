eabf6c1fb245342ae837baf42146ad04c2f04fbe
harry-up282 <143572491+harry-up282@users.noreply.github.com>
2023-12-04 20:00:42 -0800

    Update MyComponent.test.js
    complete

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -4,6 +4,37 @@ import MyComponent from './MyComponent';
 
 describe("MyComponent", () => {
   it("should render correctly", () => {
+    const wrapper = shallow(<MyComponent />);
+  });
+  it("should render initial layout", () => {
+    //when
+    const component = shallow(<MyComponent/>);
+    //then
+    expect(component.getElements()).toMatchSnapshot();
+  });
+  it("Should create an entry in component state ", ()=>{
+    //given
+    const component = shallow(<MyComponent/>);
+    const form = component.find('input');
+    //when
+    form.props().onChange({target:{
+      name: 'myName',
+      value: 'myValue'
+    }});
+    //then
+    expect(component.state('input')).toBeDefined();
+  });
+  it("should create an entry in component state with event value", ()=>{
+    //given
     const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    //when
+    form.props().onChange({target:{
+    name:'myName',
+    value: 'myValue'
+  }});
+  //then
+  expect(component.state('input')).toEqual('myValue');
   });
 });
+
